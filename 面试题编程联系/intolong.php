2621e2e5f85b9280990e6baa4c97a51dff65fa05
zht <935468215@qq.com>
2017-12-26 19:26:08 +0800

    kmp算法

--- 面试题编程联系/intolong.php
+++ 面试题编程联系/intolong.php
@@ -12,6 +12,28 @@
  * @return array
  */
 
+/**
+ *
+ * sprintf() 字符串格式化命令
+ * sprintf(format,arg1,arg2,arg++)
+ * format	必需。转换格式。
+ * arg1	必需。规定插到 format 字符串中第一个 % 符号处的参数。
+arg2	可选。规定插到 format 字符串中第二个 % 符号处的参数。
+arg++	可选。规定插到 format 字符串中第三、四等等 % 符号处的参数。
+ *
+ *  %% - 返回百分比符号
+%b - 二进制数
+%c - 依照 ASCII 值的字符
+%d - 带符号十进制数
+%e - 可续计数法（比如 1.5e+3）
+%u - 无符号十进制数
+%f - 浮点数(local settings aware)
+%F - 浮点数(not local settings aware)
+%o - 八进制数
+%s - 字符串
+%x - 十六进制数（小写字母）
+%X - 十六进制数（大写字母）
+ */
 
 function ipToArray(string $ip):array {
     return array_map(function($value){
@@ -21,25 +43,5 @@ function ipToArray(string $ip):array {
 
 var_dump(implode(ipToArray('255.0.0.1'),''));
 
-/**
- *
- * sprintf() 字符串格式化命令
- * sprintf(format,arg1,arg2,arg++)
- * format	必需。转换格式。
- * arg1	必需。规定插到 format 字符串中第一个 % 符号处的参数。
-   arg2	可选。规定插到 format 字符串中第二个 % 符号处的参数。
-   arg++	可选。规定插到 format 字符串中第三、四等等 % 符号处的参数。
- *
- *  %% - 返回百分比符号
-    %b - 二进制数
-    %c - 依照 ASCII 值的字符
-    %d - 带符号十进制数
-    %e - 可续计数法（比如 1.5e+3）
-    %u - 无符号十进制数
-    %f - 浮点数(local settings aware)
-    %F - 浮点数(not local settings aware)
-    %o - 八进制数
-    %s - 字符串
-    %x - 十六进制数（小写字母）
-    %X - 十六进制数（大写字母）
- */
+
+
---
+++ 面试题编程联系/strFind.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * KMP 字符串查找算法
+ * User: Administrator
+ * Date: 2017/12/26
+ * Time: 16:37
+ * @param string $str
+ * @param array $arr
+ */
+
+
+/**
+ * "部分匹配值"就是"前缀"和"后缀"的最长的共有元素的长度。以"ABCDABD"为例，
+ * －"A"的前缀和后缀都为空集，共有元素的长度为0；
+ * －"AB"的前缀为[A]，后缀为[B]，共有元素的长度为0；
+ * －"ABC"的前缀为[A, AB]，后缀为[BC, C]，共有元素的长度0；
+ * －"ABCD"的前缀为[A, AB, ABC]，后缀为[BCD, CD, D]，共有元素的长度为0；
+ * －"ABCDA"的前缀为[A, AB, ABC, ABCD]，后缀为[BCDA, CDA, DA, A]，共有元素为"A"，长度为1；
+ * －"ABCDAB"的前缀为[A, AB, ABC, ABCD,ABCDA]，后缀为[BCDAB, CDAB, DAB, AB, B]，共有元素为"AB"，长度为2；
+ * －"ABCDABD"的前缀为[A, AB, ABC, ABCD, ABCDA, ABCDAB]，后缀为[BCDABD,CDABD, DABD, ABD, BD, D]，共有元素的长度为0。
+ * @param string $Pstring
+ * @param array $next
+ */
+function makeNext(string $Pstring){
+
+    $m = strlen($Pstring); // 此时为数组
+    $next[0] = 0;
+    for ($q = 1, $k = 0; $q < $m; ++$q)
+    {
+        while($k > 0 && $Pstring[$q] != $Pstring[$k]){
+            $k = $next[$k-1];
+        }
+        if ($Pstring[$q] == $Pstring[$k])
+        {
+            $k++;
+        }
+        $next[$q] = $k;
+    }
+//    for ($i = 0; $i < strlen($Pstring); ++$i) {
+//        printf("%d ", $next[$i]);
+//    }
+    return $next;
+}
+
+////生成位移表
+//makeNext('abcdabd');
+
+function kmp($Tstring, $Pstring){
+
+    $n = strlen($Tstring); // 字符串
+    $m = strlen($Pstring);
+    $next= makeNext($Pstring); // 计算模式匹配表
+
+
+    for ($i=0,$q = 0; $i < $n; ++$i)
+    {
+        var_dump('q:'.$q.'\\n' );
+        var_dump('i:'.$i.'\\n' );
+        var_dump($Pstring[$q]);
+        var_dump($Tstring[$i]);
+        while($q > 0 && $Pstring[$q] != $Tstring[$i]){
+            $q = $next[$q-1];
+        }
+        if ($Pstring[$q] == $Tstring[$i])
+        {
+            $q++;
+        }
+        if ($q == $m)
+        {
+            printf("Pattern occurs with shift:%d ",($i - $m + 1));
+            printf(" You find string is:%s\n", $Pstring);
+        }
+    }
+}
+
+kmp('qweqweasdabcdabdasddfgdfgerterxgxv','abcdabd');
